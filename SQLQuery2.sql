644d2d5a111b187b50e08133f2c5b44a338c0624
Eduardo <93397077+Eduard38655@users.noreply.github.com>
2024-11-10 16:04:51 -0400

    SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,10 +10,4 @@ Eliminar  nvarchar(300)
 )
 
 INSERT INTO HomeWork(Information, Entrada, Eliminar)
-VALUES ('x', '08:30:00', '17:00:00');
-
-
-
-DELETE  FROM HomeWork WHERE ID>1
-UPDATE HomeWork SET Eliminar = 'N/A' WHERE Eliminar IS NULL
-SELECT * FROM HomeWork
+VALUES ('Values', '08:30:00', '17:00:00');
